7248c2edd53b3d6add93ea38c697a4ce2a1330e1
fedetorrecilla-oss <fede.torrecilla@gmail.com>
2026-09-20 14:07:13 -0300

    Wire exercisedb router into app-router

--- expo/backend/trpc/app-router.ts
+++ expo/backend/trpc/app-router.ts
@@ -2,11 +2,13 @@ import { createTRPCRouter } from "./create-context";
 import { crashRouter } from "./routes/crash";
 import { gymRouter } from "./routes/gym";
 import { rutinaRouter } from "./routes/rutina";
+import { exercisedbRouter } from "./routes/exercisedb";
 
 export const appRouter = createTRPCRouter({
   crash: crashRouter,
   gym: gymRouter,
   rutina: rutinaRouter,
+  exercisedb: exercisedbRouter,
 });
 
 export type AppRouter = typeof appRouter;
